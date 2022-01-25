8e665f91b827ff17e25d8c5fb62c27e6970576f8
Cledymdet <fastiil120@gmail.com>
2022-01-25 11:41:27 +0300

    Updates

---
+++ precache-manifest.b1e0fff6884fa9568c65479638645812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e7a0187b8b2eb614a2a5a6cc1d24bc",
+    "url": "/Hamming-codes/index.html"
+  },
+  {
+    "revision": "c6e93cd43b264b7a7ea0",
+    "url": "/Hamming-codes/static/css/main.14723e51.chunk.css"
+  },
+  {
+    "revision": "04e27ab0737c05f97a6b",
+    "url": "/Hamming-codes/static/js/2.2b01b504.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hamming-codes/static/js/2.2b01b504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e93cd43b264b7a7ea0",
+    "url": "/Hamming-codes/static/js/main.03a58103.chunk.js"
+  },
+  {
+    "revision": "cf9d7f6ec842c244e047",
+    "url": "/Hamming-codes/static/js/runtime-main.1c234b8b.js"
+  }
+]);
